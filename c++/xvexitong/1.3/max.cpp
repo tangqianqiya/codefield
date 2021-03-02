1cad8fd2544d8fa6d3aa173eed9bdd3eac91118a
tangqianqi <tangqianqiya@gmail.com>
2021-03-03 00:57:25 +0800

    21.3.3

--- c++/xvexitong/1.3/max.cpp
+++ c++/xvexitong/1.3/max.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 int main()
 {
-    double max3(double a, double b=1.0, double c = 2.0);
-    printf("%.2lf", max3(4.0));
+    double dmax(double a, double b=1.0, double c = 2.0);
+    printf("%.2lf", dmax(4.0));
     return 0;
 }
-double max3(double a, double b, double c)
+double dmax(double a, double b, double c)
 {
-    double max2(double a,double b);
-    double max1;
-    max1 = max2(a,b);
-    return max2(max1,c);
+    double dmax(double a,double b);
+    double imax;
+    imax = dmax(a,b);
+    return dmax(imax,c);
 }
-double max2(double a, double b)
+double dmax(double a, double b)
 {
     return a>b?a:b;
 }
